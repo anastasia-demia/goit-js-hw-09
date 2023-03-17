9b631dec03c5c10454ab941a2dab025107a56078
anastasia-demia <109302069+anastasia-demia@users.noreply.github.com>
2023-03-17 17:39:20 +0200

    task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+//
 
+const startBttn = document.querySelector("[data-start]");
+const stopBttn = document.querySelector("[data-stop]");
+const bodyColor = document.querySelector('body');
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+let timeId = null;
+startBttn.disabled = false;
+
+startBttn.addEventListener("click", () => {
+timeId = setInterval(() => {
+    startBttn.disabled = true;
+
+  bodyColor.style.backgroundColor = getRandomHexColor();
+}, 1000);
+
+});
+
+stopBttn.addEventListener("click", () => {
+clearInterval(timeId);
+startBttn.disabled = false;
+});
+
+// function centerButton(element) {
+//   element.style.position = "relative";
+//   element.style.left = "47%";
+// }
+
+// centerButton(startBtn);
+// centerButton(stopBtn);
