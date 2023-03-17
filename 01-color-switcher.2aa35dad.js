5c6cc914aa775eb388444833f30e367af4535970
anastasia-demia <109302069+anastasia-demia@users.noreply.github.com>
2023-03-17 15:39:47 +0000

    Deploying to gh-pages from @ anastasia-demia/goit-js-hw-09@9b631dec03c5c10454ab941a2dab025107a56078 🚀

---
+++ 01-color-switcher.2aa35dad.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=document.querySelector("body");var a=null;t.disabled=!1,t.addEventListener("click",(function(){a=setInterval((function(){t.disabled=!0,n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),e.addEventListener("click",(function(){clearInterval(a),t.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.2aa35dad.js.map
