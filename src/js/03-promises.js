f3f04cab5eba239b9341acfcd40dc10df6c78223
anastasia-demia <109302069+anastasia-demia@users.noreply.github.com>
2023-03-20 18:30:32 +0200

    task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,42 @@
+import { Notify } from 'notiflix';
+
+const form = document.querySelector('.form')
+form.addEventListener('submit', onFormSubmit);
+
+
+function onFormSubmit (e){
+  e.preventDefault();
+
+  let {delay, step, amount} = e.currentTarget;
+  delay= Number(delay.value);
+  step= Number(step.value);
+  amount= Number(amount.value);
+
+for (let position =1; position <= amount; position+=1){
+ createPromise(position, delay)
+.then(onMakeOrderSuccess)
+.catch(onMakeOrderError)
+delay += step;}
+}
+
+const onMakeOrderSuccess =({ position, delay }) =>{
+    Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`)
+  }
+const onMakeOrderError = ({ position, delay }) =>{
+    Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`)
+  }
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+    if (shouldResolve) {
+      resolve({ position, delay }
+      );
+    } else {
+      reject({ position, delay }
+      );
+    }
+  }, delay);
+  });
 }
